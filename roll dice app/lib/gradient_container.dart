af0e95e796ad9b55190eb7753edaa1ea0e6ca3e8
divya-rosy <drthomas5432@gmail.com>
2025-04-24 11:21:49 +0530

    implementing reusable widgets

--- roll dice app/lib/gradient_container.dart
+++ roll dice app/lib/gradient_container.dart
@@ -1,12 +1,29 @@
+import 'package:first_app/styled_text.dart';
+
 import 'package:flutter/material.dart';
 
-import 'package:first_app/main.dart';
-void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        body: GradientContainer(),
+var startAlignment = Alignment.topLeft;
+var endAlignment = Alignment.bottomRight;
+
+class GradientContainer extends StatelessWidget {
+  const GradientContainer(this.color1, this.color2, {super.key});
+
+  final Color color1;
+  final Color color2;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [color1, color2],
+          begin: startAlignment,
+          end: endAlignment,
+        ),
       ),
-    ),
-  );
+      child: const Center(
+        child: StyledText('Hello World!'),
+      ),
+    );
+  }
 }
--- roll dice app/lib/main.dart
+++ roll dice app/lib/main.dart
@@ -1,31 +1,13 @@
-import 'package:first_app/styled_text.dart';
 import 'package:flutter/material.dart';
 
-var startAlignment = Alignment.topLeft;
-var endAlignment = Alignment.bottomRight;
+import 'package:first_app/gradient_container.dart';
+
 void main() {
-  runApp(MaterialApp(home: Scaffold(body: GradientContainer())));
-}
-
-class GradientContainer extends StatelessWidget {
-  const GradientContainer({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        gradient: LinearGradient(
-          colors: const [
-            Color.fromARGB(255, 68, 98, 120),
-            Color.fromARGB(205, 70, 59, 123),
-          ],
-          begin: startAlignment,
-          end: endAlignment,
-        ),
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: const GradientContainer(Color.fromARGB(255, 49, 137, 209), Color.fromARGB(255, 29, 161, 131)),
       ),
-      child: const Center(
-        child: StyledText('Hello World!'),
-      ),
-    );
-  }
+    ),
+  );
 }
