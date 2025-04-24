5066bc44e8c346d6dbc18d0b99a2b02a9ad9f4da
divya-rosy <drthomas5432@gmail.com>
2025-04-24 09:59:00 +0530

    Initial commit

---
+++ roll dice app/lib/gradient_container.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+import 'package:first_app/main.dart';
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: GradientContainer(),
+      ),
+    ),
+  );
+}
---
+++ roll dice app/lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:first_app/styled_text.dart';
+import 'package:flutter/material.dart';
+
+var startAlignment = Alignment.topLeft;
+var endAlignment = Alignment.bottomRight;
+void main() {
+  runApp(MaterialApp(home: Scaffold(body: GradientContainer())));
+}
+
+class GradientContainer extends StatelessWidget {
+  const GradientContainer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: const [
+            Color.fromARGB(255, 68, 98, 120),
+            Color.fromARGB(205, 70, 59, 123),
+          ],
+          begin: startAlignment,
+          end: endAlignment,
+        ),
+      ),
+      child: const Center(
+        child: StyledText('Hello World!'),
+      ),
+    );
+  }
+}
